f63edda13f1c14180735500333a1d761d38da4f0
SSRemex <769885884@qq.com>
2023-01-16 18:26:18 +0800

    前端

---
+++ front/src/page/router.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+
+class RouterConfig extends React.Component {
+  render(): React.ReactNode {
+    return <HashRouter>
+        dafsd
+    </HashRouter>;
+  }
+}
+
+export default RouterConfig;
