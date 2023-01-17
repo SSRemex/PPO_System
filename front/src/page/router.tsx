3afee6f793e75b40b4a33b2e9cf39b1fab9c5635
SSRemex <769885884@qq.com>
2023-01-17 15:54:48 +0800

    路由框架搭建完成

--- front/src/page/router.tsx
+++ front/src/page/router.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import {HashRouter,Route, Switch} from 'react-router-dom';
 
 class RouterConfig extends React.Component {
   render(): React.ReactNode {
-    return <HashRouter>
-        dafsd
-    </HashRouter>;
+    return (
+      <HashRouter>
+        {/* 注册路由 */}
+        <Switch>
+          <Route exact path="/" render={()=> {return (<div> home </div>)}} />
+          <Route path="/about" render={()=> {return (<div> about </div>)}} />
+          <Route path="/topics" render={()=> {return (<div> topics </div>)}} />
+        </Switch>
+      </HashRouter>
+    );
   }
 }
 
