d49bf59efc3d6b199c219ea9e2875c5524c96024
SSRemex <769885884@qq.com>
2023-02-13 21:57:30 +0800

    新前端

---
+++ web/src/config/menuConfig.js
@@ -0,0 +1,25 @@
+/**
+ * icon:菜单项图标
+ * roles:标明当前菜单项在何种角色下可以显示，如果不写此选项，表示该菜单项完全公开，在任何角色下都显示
+ */
+const menuList = [
+  {
+    title: "首页",
+    path: "/dashboard",
+    icon: "home",
+    roles:["admin","editor","guest"]
+  },
+  {
+    title: "表格",
+    path: "/table",
+    icon: "table",
+    roles:["admin","editor"]
+  },
+  {
+    title: "用户管理",
+    path: "/user",
+    icon: "usergroup-add",
+    roles:["admin"]
+  }
+];
+export default menuList;
---
+++ web/src/views/dashboard/index.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Row, Col } from "antd";
+import "./index.less";
+
+
+
+const Dashboard = () => {
+
+
+  return (
+    <div className="app-container">
+      <a
+        href="https://github.com/NLRX-WJC/react-antd-admin-template"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="github-corner"
+      ></a>
+
+
+
+
+      
+    </div>
+  );
+};
+
+export default Dashboard;
